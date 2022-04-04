820522b42fd20447efe77d280b91bec1405cbbff
Jan_Boh <33293477+bohnertj@users.noreply.github.com>
2022-04-05 00:19:04 +0200

    Update server.js
    add salaries router

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.use(errorHandler);
 const invoicesRoutes = require ('./incoming/incoming.controller')
 app.use('/api/v1/invoice', invoicesRoutes);
 
+const salariesRoutes = require ('./outcoming/outcoming.controller')
+app.use('/api/v1/salary', salariesRoutes);
+
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
