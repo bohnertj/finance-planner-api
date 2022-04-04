4663466dda2603dc7de812b71fee6bb3fce0af66
Jan_Boh <33293477+bohnertj@users.noreply.github.com>
2022-04-05 00:16:58 +0200

    Add outcomming controller

---
+++ outcomming/outcomming.controller.js
@@ -0,0 +1,242 @@
+const express = require('express');
+const router = express.Router();
+const salary = require('../incoming/incoming.model');
+var MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const incomingModel = require('../incoming/incoming.model');
+const salary = express();
+const incomingService = require('../incoming/incoming.service');
+const { mquery } = require('mongoose');
+salary.use(bodyParser.json());
+var ObjectId = require('mongodb').ObjectID;
+
+
+var url = "mongodb+srv://root:example@cluster0.oltk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+router.delete('/:_id', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
+    console.log('Löschung wird aufgerufen, mit id' + req.params._id)
+    MongoClient.connect(url,function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      dbo.collection("customers").deleteOne({ _id: ObjectId(req.params._id) }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log('Löschung wird aufgerufen')
+    res.json({ message: err })
+  }
+});
+
+
+
+
+
+//ROUTES
+router.get('/', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log("Ich war hier");
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      console.log("Header: " + req.headers.username);
+      dbo.collection("customers").find({ username: req.headers.username }).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log("error: " + err);
+    res.json({ message: err })
+  }
+});
+
+router.get('/categories', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      dbo.collection("customers").aggregate([{ $match: { username: req.headers.username } }, { $group: { _id: "$categorie", amount: { $push: "$$ROOT" } } }, {
+        $addFields:
+        {
+          amount: { $sum: "$amount.amount" }
+        }
+      }]).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log("error: " + err);
+    res.json({ message: err })
+  }
+});
+
+router.get('/salarybydate', async (req, res) => {
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      var start = new Date("2022-03-30");
+      var end = new Date("2021-03-01");
+      dbo.collection("customers").aggregate([{
+        $match: { username: req.headers.username }
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$date" },
+            month: { $month: "$date" },
+          }, amount: { $push: "$$ROOT" }
+        }
+      }, {
+        $addFields:
+        {
+          amount: { $sum: "$amount.amount" }
+        }
+      }]).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log("error: " + err);
+    res.json({ message: err })
+  }
+});
+
+
+
+router.get('/test', async (req, res) => {
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      var start = new Date("2022-03-30");
+      var end = new Date("2020-02-01");
+      dbo.collection("customers").aggregate([{
+        $match: { // filter to limit to whatever is of importance
+          "date": {
+            $gte: end,
+            $lte: start
+          }
+        }
+      }, {
+        $group: { // group by
+          _id: {
+            "month": { $month: "$date" }, // month
+            "year": { $year: "$date" }
+          }, // and year
+           amount: { $push: "$$ROOT" }
+         // "count": { $sum: 1 }  // and sum up all documents per group
+        }
+      }]).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log("error: " + err);
+    res.json({ message: err })
+  }
+});
+
+router.get('/:title', async (req, res) => {
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+      dbo.collection("customers").findOne({ "title": req.params.title }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    res.json({ message: err })
+  }
+});
+
+router.post('/', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  const salary = new salary({
+    title: req.body.title,
+    categorie: req.body.categorie,
+    amount: req.body.amount,
+    username: req.body.username,
+    date: req.body.date
+  });
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("salary");
+    dbo.collection("customers").insertOne(salary, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+    res.json(salary);
+  });
+});
+
+router.put('/:_id', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  try {
+    console.log('Update wird aufgerufen mit id' + req.params._id)
+    console.log('neuer titel' + req.body.title);
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("salary");
+
+      var myquery = { _id: ObjectId(req.params._id) };
+      var rawdate = req.body.date
+
+      //create a new Date object
+      var date = new Date(rawdate)
+      var newvalues = { $set: { title: req.body.title, amount: req.body.amount, categorie: req.body.categorie, date: date } };
+      dbo.collection("customers").updateOne(myquery, newvalues, function (err, result) {
+        if (err) throw err;
+        console.log("1 document updated");
+        res.json(result);
+        db.close();
+      });
+    });
+  }
+  catch (err) {
+    console.log('Löschung wird aufgerufen')
+    res.json({ message: err })
+  }
+});
+
+
+
+
+module.exports = router;
